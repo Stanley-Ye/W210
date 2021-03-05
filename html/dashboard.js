7ac3e517b8b0d3d8ab9fa5f0f1ccf8264b8f0cbb
Stanley Ye <stanley.ye@berkeley.edu>
2021-03-04 18:28:24 -0800

    Initial webpage

---
+++ html/dashboard.js
@@ -0,0 +1,43 @@
+function submit(){
+  var textbox = document.getElementById("textbox");
+  var response = document.getElementById("response");
+  response.value = "HI:" + textbox.value;
+}
+
+function displayResponse(text){
+  var response = document.getElementById("response");
+  response.value = text;
+}
+
+function displayResponse2(text){
+  var response2 = document.getElementById("response2");
+  response2.value = text;
+}
+
+function submit2(){
+var myHeaders = new Headers();
+myHeaders.append("x-api-key", "G0W5V6A6WB9JcVms3IKxN5TVSYCVZX7z7IX5KgnT");
+myHeaders.append("Content-Type", "application/json");
+var raw = JSON.stringify({"in_txt":"It was the best of times","author_name":"Jane Austen"});
+  
+  var requestOptions = {
+  method: 'POST',
+  headers: myHeaders,
+  body: raw,
+  redirect: 'follow'
+};
+
+	console.log("BF");
+  
+var content = document.getElementById('response');
+
+  
+  fetch("https://naocgyef4k.execute-api.us-east-2.amazonaws.com/default/stylometry_beta?chunk_size=128", requestOptions)
+  .then(response => response.text())
+  .then(result => console.log(result))
+  .catch(error => content.innerHTML = error.message);
+  
+  
+  console.log("AF");
+  displayResponse2("hello");
+}
