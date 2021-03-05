4ab6065b91ff5f8fd88131a65aa6bb57567b9784
Stanley Ye <stanley.ye@berkeley.edu>
2021-03-05 13:48:49 -0800

    My postman

--- html/dashboard.js
+++ html/dashboard.js
@@ -16,10 +16,14 @@ function displayResponse2(text){
 
 function submit2(){
   var myHeaders = new Headers();
+  myHeaders.append("x-api-key", "G0W5V6A6WB9JcVms3IKxN5TVSYCVZX7z7IX5KgnT");
   myHeaders.append("Content-Type", "application/json");
+
+
   var raw = JSON.stringify({"in_txt":textbox.value,"author_name":author.value});
   console.log("DEBUG: ");
   console.log(raw);
+  
   var requestOptions = {
     method: 'POST',
     headers: myHeaders,
@@ -31,10 +35,15 @@ function submit2(){
   var content = document.getElementById('response');
   var content2 = document.getElementById('response2');
 
-  fetch("https://cjyiruowfd.execute-api.us-east-2.amazonaws.com/default/stylometry_beta?chunk_size=128", requestOptions)
-    .then(response => content.innerHTML = response.text())
-    .then(result => content2.innerHTML = JSON.stringify(result))
-    .catch(error => console.log('error', error));
+  fetch("https://naocgyef4k.execute-api.us-east-2.amazonaws.com/default/stylometry_beta?chunk_size=128", requestOptions)
+  .then(response => response.text())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));
+
+//  fetch("https://cjyiruowfd.execute-api.us-east-2.amazonaws.com/default/stylometry_beta?chunk_size=128", requestOptions)
+//    .then(response => content.innerHTML = response.text())
+//    .then(result => content2.innerHTML = JSON.stringify(result))
+//    .catch(error => console.log('error', error));
   
   
   console.log("END");
