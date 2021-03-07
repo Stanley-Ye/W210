13bfcdcdbed8f02800a7049e6e05bf3afd358fd2
Stanley Ye <stanley.ye@berkeley.edu>
2021-03-06 22:22:39 -0800

    Added test key

--- html/dashboard.js
+++ html/dashboard.js
@@ -1,50 +1,28 @@
-function submit(){
-  var textbox = document.getElementById("textbox");
-  var response = document.getElementById("response");
-  response.value = "HI:" + textbox.value;
+//import React, { Component } from 'react';
+//console.log(process.env.REACT_APP_API_KEY_stylometry);
+
+require('dotenv').config()
+const key1 = process.env.KEY_bert;
+const key2 = process.env.KEY_stylometry;
+console.log(key1);
+console.log(key2);
+
+
+
+const result = dotenv.config()
+ 
+if (result.error) {
+  throw result.error
+}
+ 
+console.log(result.parsed)
+
+
+
+function submit_bert(){
+  console.log("submit_bert");
 }
 
-function displayResponse(text){
-  var response = document.getElementById("response");
-  response.value = text;
-}
-
-function displayResponse2(text){
-  var response2 = document.getElementById("response2");
-  response2.value = text;
-}
-
-function submit2(){
-  var myHeaders = new Headers();
-  myHeaders.append("x-api-key", "G0W5V6A6WB9JcVms3IKxN5TVSYCVZX7z7IX5KgnT");
-  myHeaders.append("Content-Type", "application/json");
-
-  var raw = JSON.stringify({"in_txt":textbox.value,"author_name":author.value});
-  console.log("DEBUG: ");
-  console.log(raw);
-  
-  var requestOptions = {
-    method: 'POST',
-    headers: myHeaders,
-    body: raw,
-    redirect: 'follow'
-  };
-
-
-  var content = document.getElementById('response');
-  var content2 = document.getElementById('response2');
-
-  fetch("https://naocgyef4k.execute-api.us-east-2.amazonaws.com/default/stylometry_beta?chunk_size=128", requestOptions)
-  .then(response => response.text())
-  .then(result => console.log(result))
-  .catch(error => console.log('error', error));
-
-//  fetch("https://cjyiruowfd.execute-api.us-east-2.amazonaws.com/default/stylometry_beta?chunk_size=128", requestOptions)
-//    .then(response => content.innerHTML = response.text())
-//    .then(result => content2.innerHTML = JSON.stringify(result))
-//    .catch(error => console.log('error', error));
-  
-  
-  console.log("END");
-  //displayResponse2("hello-2");
+function submit_stylometry(){
+  console.log("submit_stylometry");
 }
